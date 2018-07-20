11b7ef5a72695b8b0b4fd953e7d60ebc9a7648cf
Bdoner <bdoner@noreply.user.github.com>
2018-07-20 22:51:27 +0200

    SetForeGroundWindow was the magic function

---
+++ Externals.DllImports.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Minesweeper
+{
+    public partial class Externals
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(HandleRef hWnd, out RECT lpRect);
+
+        
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, SetWindowPosFlags uFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool BringWindowToTop(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,9 +15,10 @@ namespace Minesweeper
 
             var rect = new RECT();
             var rectSuccess = GetWindowRect(new HandleRef(default(object), gameWindow), out rect);
+            //Get window size
 
-            //var placement = new WINDOWPLACEMENT();
-            //var placementSuccess = GetWindowPlacement(gameWindow, ref placement);
+            var topSuccess = SetForegroundWindow(gameWindow);// SetWindowPos(gameWindow, (IntPtr)0, 0, 0, 0, 0, SetWindowPosFlags.IgnoreMove | SetWindowPosFlags.IgnoreResize | SetWindowPosFlags.ShowWindow);
+            //Bring to top
 
             var windowPicture = new Bitmap(rect.Right - rect.Left, rect.Bottom - rect.Top);
             using (var g = Graphics.FromImage(windowPicture))
@@ -25,6 +26,7 @@ namespace Minesweeper
                 g.CopyFromScreen(new Point(rect.Left, rect.Top), Point.Empty, new Size(rect.Right - rect.Left, rect.Bottom - rect.Top));
 
             }
+            windowPicture.Save("C:\\tmp\\ms.bmp");
             //Grab picture
 
 
@@ -36,6 +38,6 @@ namespace Minesweeper
             //  mark mines
             // - Repeat main loop
         }
-        
+
     }
 }
