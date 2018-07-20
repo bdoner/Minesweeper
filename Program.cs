f0331e72b0af6acfdee6684f53d7049fef2d8936
Bdoner <bdoner@noreply.user.github.com>
2018-07-20 21:48:34 +0200

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using static Minesweeper.Externals;
 
 namespace Minesweeper
 {
@@ -10,6 +9,33 @@ namespace Minesweeper
     {
         static void Main(string[] args)
         {
+            IntPtr gameWindow = FindWindow(default(string), "Minesweeper");
+            HandleRef gameWindowHandle = new HandleRef(default(object), gameWindow);
+            //Get window
+
+            var rect = new RECT();
+            var rectSuccess = GetWindowRect(new HandleRef(default(object), gameWindow), out rect);
+
+            //var placement = new WINDOWPLACEMENT();
+            //var placementSuccess = GetWindowPlacement(gameWindow, ref placement);
+
+            var windowPicture = new Bitmap(rect.Right - rect.Left, rect.Bottom - rect.Top);
+            using (var g = Graphics.FromImage(windowPicture))
+            {
+                g.CopyFromScreen(new Point(rect.Left, rect.Top), Point.Empty, new Size(rect.Right - rect.Left, rect.Bottom - rect.Top));
+
+            }
+            //Grab picture
+
+
+            //Get board rect
+            // - main loop
+            //  picture board
+            //  parse board
+            //  find mines
+            //  mark mines
+            // - Repeat main loop
         }
+        
     }
 }
