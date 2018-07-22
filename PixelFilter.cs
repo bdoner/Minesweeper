f8b8aaefd889484f3f58b5d885323f2a8c374fcc
Bdoner <bdoner@noreply.user.github.com>
2018-07-22 22:06:58 +0200

    Added a pixelfilter to not just match a single pixel but match a cluster of pixels

---
+++ PixelFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public struct Pixel
+    {
+        public int RelX;
+        public int RelY;
+        public byte ColorVal;
+    }
+
+    public class PixelFilter
+    {
+        public List<Pixel> Filter { get; set; }
+        public int Threshold { get; set; } = 0;
+        public bool IsMatch(ref Bitmap bitmap, int sourceX, int sourceY)
+        {
+            foreach (var f in Filter)
+            {
+                var fX = sourceX + f.RelX;
+                if (fX < 0) return false;
+                if (fX >= bitmap.Width) return false;
+
+                var fY = sourceY + f.RelY;
+                if (fY < 0) return false;
+                if (fY >= bitmap.Height) return false;
+
+                var p = bitmap.GetPixel(fX, fY);
+                var minCol = Math.Max(0, Math.Min(255, f.ColorVal - Threshold));
+                var maxCol = Math.Max(0, Math.Min(255, f.ColorVal + Threshold));
+                if (!(p.G > minCol && p.G < maxCol))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Threading;
 using static Minesweeper.Externals;
 
 namespace Minesweeper
@@ -10,6 +12,8 @@ namespace Minesweeper
         static void Main(string[] args)
         {
             IntPtr gameWindow = FindWindow(default(string), "Minesweeper");
+            if (gameWindow == IntPtr.Zero) Environment.Exit(1);
+
             HandleRef gameWindowHandle = new HandleRef(default(object), gameWindow);
             //Get window
 
@@ -26,10 +30,105 @@ namespace Minesweeper
                 g.CopyFromScreen(new Point(rect.Left, rect.Top), Point.Empty, new Size(rect.Right - rect.Left, rect.Bottom - rect.Top));
 
             }
-            windowPicture.Save("C:\\tmp\\ms.bmp");
+            windowPicture.Save("ms.bmp");
             //Grab picture
 
+            int borderCount = 0;
+            var borderFilter = new PixelFilter
+            {
+                Threshold = 32,
+                Filter = new List<Pixel>
+                {
+                    //Above
+                    new Pixel
+                    {
+                        ColorVal = 0x06,
+                        RelX = -1,
+                        RelY = -1
+                    },
+                    new Pixel
+                    {
+                        ColorVal = 0x0f,
+                        RelX = 0,
+                        RelY = -1
+                    },
+                    new Pixel
+                    {
+                        ColorVal = 0x0e,
+                        RelX = 1,
+                        RelY = -1
+                    },
 
+                    //Same row
+                    new Pixel
+                    {
+                        ColorVal = 0x0c,
+                        RelX = -1,
+                        RelY = 0
+                    },
+                    //new Pixel
+                    //{
+                    //    ColorVal = 0x64,
+                    //    RelX = 0,
+                    //    RelY = 0
+                    //},
+                    //new Pixel
+                    //{
+                    //    ColorVal = 0x7a,
+                    //    RelX = 1,
+                    //    RelY = 0
+                    //},
+
+                    //Below
+                    new Pixel
+                    {
+                        ColorVal = 0x0e,
+                        RelX = -1,
+                        RelY = 1
+                    },
+                    //new Pixel
+                    //{
+                    //    ColorVal = 0x78,
+                    //    RelX = 0,
+                    //    RelY = 1
+                    //},
+                    //new Pixel
+                    //{
+                    //    ColorVal = 0xac,
+                    //    RelX = 1,
+                    //    RelY = 1
+                    //},
+
+                    //Check opposite
+                    new Pixel
+                    {
+                        ColorVal = 0x02,
+                        RelX = 30,
+                        RelY = 30
+                    }
+                }
+            };
+
+            var g2 = Graphics.FromImage(windowPicture);
+            for (var y = 0; y < windowPicture.Height; y++)
+            {
+                for (var x = 0; x < windowPicture.Width; x++)
+                {
+                    var pixel = windowPicture.GetPixel(x, y);
+                    if (borderFilter.IsMatch(ref windowPicture, x, y))
+                    {
+                        borderCount++;
+                        Console.WriteLine($"Found border at ({x}, {y})");
+                        g2.DrawRectangle(Pens.Red, x, y, 29, 29);
+
+                        x += 25;
+                    }
+                }
+            }
+            windowPicture.Save("ms_drawn.bmp");
+            g2.Dispose();
+
+            Console.WriteLine($"Found {borderCount} border-pixels");
             //Get board rect
             // - main loop
             //  picture board
@@ -37,6 +136,10 @@ namespace Minesweeper
             //  find mines
             //  mark mines
             // - Repeat main loop
+
+            Console.WriteLine("Done...");
+            Thread.Sleep(5000);
+            //Console.ReadLine();
         }
 
     }
